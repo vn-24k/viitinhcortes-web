bfe0aee3b9f4f0c41b565529a69492817f27b38f
vn-24k <manuellesilva0305@gmail.com>
2026-05-20 23:05:47 -0300

    fix: final stable build configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    // Isso desativa o lightningcss que está implicando com o @tailwind
-    transformer: 'postcss',
-  },
   build: {
     minify: 'esbuild',
+    rollupOptions: {
+      external: []
+    }
   }
 })
